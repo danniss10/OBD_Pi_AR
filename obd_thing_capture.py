d62b29c50d5cf5b7b52d6ee0cf8bc5e1d68513be
Daniel Nissenbaum <danniss10@Daniels-MBP-2.ptcnet.ptc.com>
2016-07-28 10:06:56 -0400

    First commit

---
+++ obd_thing_capture.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+###########################################################################
+# obd_thing_capture.py
+#
+# Created by Paul Bartek (pbartek@cowfishstudios.com)
+# Modified by Daniel Nissenbaum (dnissenbaum@ptc.com)
+#
+# Returns recorded OBD2 data as a dictionary
+#
+###########################################################################
+
+import obd_io
+import serial
+import platform
+import obd_sensors
+from datetime import datetime
+import time
+import getpass
+
+
+from obd_utils import scanSerial
+
+class OBD_Recorder():
+    def __init__(self, log_items):
+        
+        # set up file and empty port
+        self.port = None
+        self.sensorlist = []
+
+        for item in log_items:
+            self.add_log_item(item)
+
+        self.gear_ratios = [34/13, 39/21, 36/23, 27/20, 26/21, 25/22]
+        #log_formatter = logging.Formatter('%(asctime)s.%(msecs).03d,%(message)s', "%H:%M:%S")
+
+    def connect(self):
+        portnames = scanSerial()
+        #portnames = ['COM10']
+        print portnames
+        for port in portnames:
+            self.port = obd_io.OBDPort(port, None, 2, 2)
+            if(self.port.State == 0):
+                self.port.close()
+                self.port = None
+            else:
+                break
+
+        if(self.port):
+            print "Connected to "+self.port.port.name
+            
+    def is_connected(self):
+        return self.port
+        
+    def add_log_item(self, item):
+        for index, e in enumerate(obd_sensors.SENSORS):
+            if(item == e.shortname):
+                self.sensorlist.append(index)
+                print "Logging item: "+e.name
+                break
+            
+            
+    def get_data(self):
+        if(self.port is None):
+            return None
+        
+        print "Logging started"
+
+        localtime = datetime.now()
+        current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)+"."+str(localtime.microsecond)
+        log_string = current_time
+        results = {}
+        for index in self.sensorlist:
+            (name, value, unit) = self.port.sensor(index)
+            log_string = log_string + ", "+str(value)
+            results[obd_sensors.SENSORS[index].shortname] = value;
+
+        gear = self.calculate_gear(results["rpm"], results["speed"])
+        log_string = log_string + "," + str(gear)
+
+        #print log_string + "\n"
+
+        return results
+
+            
+    def calculate_gear(self, rpm, speed):
+        if speed == "" or speed == 0:
+            return 0
+        if rpm == "" or rpm == 0:
+            return 0
+
+        rps = rpm/60
+        mps = (speed*1.609*1000)/3600
+        
+        primary_gear = 85/46 #street triple
+        final_drive  = 47/16
+        
+        tyre_circumference = 1.978 #meters
+
+        current_gear_ratio = (rps*tyre_circumference)/(mps*primary_gear*final_drive)
+        
+        #print current_gear_ratio
+        gear = min((abs(current_gear_ratio - i), i) for i in self.gear_ratios)[1] 
+        return gear
+        
+
---
+++ thingxpi.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# title : thingxpi.py
+# author: Daniel Nissenbaum
+#
+# This is a Python script to gather readings on the Raspberry Pi and connected
+# sensors and update the properties for a 'thing' on a ThingWorx instance.
+#
+# This particular script gets data from a connected OBD2 port
+#
+# From the command line run,
+# './thingxpi.py <thing> <ThingWorx url> <Application Key>
+#
+#
+
+
+import requests
+import sys
+import json
+import time
+import subprocess
+import obd_thing_capture as obd_data
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings()
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Info to connect to Thingworx
+thing = sys.argv[1]
+url = sys.argv[2]
+appKey = sys.argv[3]
+
+# Header data
+headers = {'Content-Type': 'application/json', 'appKey': appKey}
+
+# set up OBD capturing
+logitems = ["rpm", "speed", "throttle_pos", "load", "fuel_status"]
+o = obd_data.OBD_Recorder(logitems)
+o.connect()
+
+if not o.is_connected():
+    print "Not connected"
+# o.get_data()
+
+while True:
+
+    payload = o.get_data()
+
+    print payload
+
+    response = requests.put(url + '/Things/' + thing + '/Properties/*',
+                            headers=headers, json=payload, verify=False)
+
+    print response
+
+    time.sleep(1)
+    print '========================='
+    
